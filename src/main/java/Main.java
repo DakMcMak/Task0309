b9c750c1ec59b93c05f59eb69f273665d0bdfd4f
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-19 22:36:24 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        int summa = 1;
+        while (i < 11){
+            System.out.println(summa);
+            i++;
+            summa = summa + i;
+        }
+    }
+}
